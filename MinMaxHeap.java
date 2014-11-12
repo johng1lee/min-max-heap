c0b00f89248ad88ba42e1bda23b4e323291d6d17
lee <leejohng1@gmail.com>
2014-11-12 14:45:48 -0800

    implemented array structured heap

--- MinMaxHeap.java
+++ MinMaxHeap.java
@@ -2,29 +2,16 @@ import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 public class MinMaxHeap{
-    private Node root;
+    private ArrayList<Integer> heap;
     private int heapSize;
-    private Node tmpNode;
     public MinMaxHeap(){
-	root = null;
+	heap = new ArrayList<Integer>();
 	heapSize = 0;
-	tmpNode = null;
     }
 
     public void insert(int element){
-	// root.value = element;
-	// root.left = new Node(24);
-	// root.right = new Node(15);
-	Node elementNode = new Node(element);
-	if(root == null){
-	    root = elementNode;
-	    heapSize++;
-	}
-	else if(){
-	    
-	}
-	else{
-	    int levelIndex = heapSize + 1;
+	if(heap.size() == 0){
+	    heap.add(element);
 	}
     }
     public void buildMinMaxHeap(int[] initialValues){
@@ -35,18 +22,15 @@ public class MinMaxHeap{
     }
     public int deleteMin(){return 0;}
     public int deleteMax(){return 0;}
-    public void printMinMaxHeap(){}
+    public void printMinMaxHeap(){printLevels();}
     public void printLevels(){
-	Stack<Node> nodeStack = new Stack<Node>();
-	long counter = 0;
+	int counter = 0;
 	long limit = 1;
-	if(root == null){return;}
+	if(heap.size() < 1){return;}
 	else{
-	    nodeStack.push(root);
-	    while(!nodeStack.empty()){
-		Node evaluateNode = (Node)nodeStack.pop();
+	    while(counter < heap.size()){
+		System.out.print(heap.get(counter));
 		counter++;
-		System.out.print(evaluateNode.value);
 		if(counter >= limit){
 		    System.out.println("");
 		    limit = limit + limit * 2;
@@ -54,69 +38,44 @@ public class MinMaxHeap{
 		else{
 		    System.out.print(", ");
 		}
-		if(evaluateNode.left != null){
-		    nodeStack.push(evaluateNode.left);
-		}
-		if(evaluateNode.right != null){
-		    nodeStack.push(evaluateNode.right);
-		}
 	    }
 	}
 	System.out.println("");
     }
 
     public int peekMin(){
-	if(root == null){
+	if(heap.size() <= 0){
 	    System.err.println("Heap is empty");
 	    return 0;
 	}
 	else{
-	    System.out.println(root.value);
-	    return root.value;
+	    System.out.println(heap.get(0));
+	    return heap.get(0);
 	}
     }
     public int peekMax(){
-	Node maxNode = root.getMax(root);
-	if(maxNode == null){
+	if(heap.size() < 1){
+	    System.err.println("Heap is empty");
+	    return 0;
+	}
+	else if(heap.size() < 2){
 	    System.err.println("Heap does not have a max");
+	    return 0;
+	}
+	else if(heap.size() < 3){
+	    System.out.println(heap.get(1));
+	    return heap.get(1);
 	}
 	else{
-	    System.out.println(maxNode.value);
-	}
-	return maxNode.value;
-    }
-
-    public class Node{
-	public Node left = null;
-	public Node right = null;
-	public int value;
-	public Node(){
-	}
-	public Node(int elementValue){
-	    value = elementValue;
-	}
-	public Node getMax(Node parent){
-	    if(parent == null){
-		return null;
+	    int left = heap.get(1);
+	    int right = heap.get(2);
+	    if(left < right){
+		System.out.println(right);
+		return right;
 	    }
 	    else{
-		if(root.left == null && root.right == null){
-		    return null;
-		}
-		else if(root.left == null){
-		    return root.right;
-		}
-		else if(root.right == null){
-		    return root.left;
-		}
-		else{
-		    if(root.left.value >= root.right.value){
-			return root.left;
-		    }
-		    else{
-			return root.right;
-		    }
-		}
+		System.out.println(left);
+		return left;
 	    }
 	}
     }
